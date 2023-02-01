08fdbdfe18a3ac2e202ebc1edf3274b837a7ec6b
Harish07032003 <harish732003@gmail.com>
2023-02-01 23:18:16 +0530

    Create min_consecutive_cards_to_pick_up.java

---
+++ GOLDMAN SACHS/min_consecutive_cards_to_pick_up.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class min_consecutive_cards_to_pick_up {
+    public int minimumCardPickup(int[] cards) {
+        int count=Integer.MAX_VALUE;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<cards.length;i++)
+        {
+            if(!map.containsKey(cards[i]))
+            {
+                map.put(cards[i],i);
+            }
+            else
+            {
+                if(i-map.get(cards[i])<count)
+                {
+                    count=i-map.get(cards[i]);
+                }
+                map.put(cards[i],i);
+            }
+        }
+            if(count==Integer.MAX_VALUE)
+            {
+                return -1;
+            }
+        return count+1;
+    }
+}
