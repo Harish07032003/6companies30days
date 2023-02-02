f4cab14ce8efdc8f466b94114360493997fd9170
Harish07032003 <harish732003@gmail.com>
2023-02-02 21:02:42 +0530

    Create Number_of_Matching_Subsequences.java

---
+++ AMAZON/Number_of_Matching_Subsequences.java
@@ -0,0 +1,35 @@
+package AMAZON;
+import java.util.*;
+public class Number_of_Matching_Subsequences {
+    public int numMatchingSubseq(String s, String[] words) {
+        int a=0;
+        int res=0;
+        while(a<words.length)
+        {
+            String arr[]=s.split("");
+            String st=words[a];
+            int count=0;
+            for(int i=0;i<st.length();i++)
+            {
+                for(int j=i;j<arr.length;j++)
+                {
+                    if((st.charAt(i)+"").equals(arr[j]))
+                    {
+                        arr[j]="*";
+                        count++;
+                        // System.out.println(st+","+count+"-"+Arrays.toString(arr));
+                        break;
+                    }
+                }
+            }
+            if(count==st.length())
+            {
+                // System.out.println(st+","+res);
+                res++;
+            }
+            a++;
+        }
+        
+        return res;
+    }
+}
